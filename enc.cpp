fe8c1bfb523f47144023ac926b30e77eeaa13968
ctrlaltdev <yorick@ctrlalt.dev>
2019-12-01 23:30:52 -0800

    Tada

---
+++ enc.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+
+#include <cryptopp/modes.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/filters.h>
+
+int encrypt(std::string plaintext) {
+    //Key and IV setup
+    //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-   
+    //bit). This key is secretly exchanged between two parties before communication   
+    //begins. DEFAULT_KEYLENGTH= 16 bytes
+    CryptoPP::byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ], iv[ CryptoPP::AES::BLOCKSIZE ];
+    memset( key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH );
+    memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
+
+    std::string ciphertext;
+
+    //
+    // Create Cipher Text
+    //
+    CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
+
+    CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink( ciphertext ) );
+    stfEncryptor.Put( reinterpret_cast<const unsigned char*>( plaintext.c_str() ), plaintext.length() );
+    stfEncryptor.MessageEnd();
+
+    //
+    // Dump Cipher Text
+    //
+
+    for( int i = 0; i < ciphertext.size(); i++ ) {
+        std::cout << static_cast<CryptoPP::byte>(ciphertext[i]);
+    }
+
+    return 0;
+}
+
+int decrypt(std::string ciphertext) {
+    //Key and IV setup
+    //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-   
+    //bit). This key is secretly exchanged between two parties before communication   
+    //begins. DEFAULT_KEYLENGTH= 16 bytes
+    CryptoPP::byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ], iv[ CryptoPP::AES::BLOCKSIZE ];
+    memset( key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH );
+    memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
+
+    std::string decryptedtext;
+
+    //
+    // Decrypt
+    //
+    CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption( aesDecryption, iv );
+
+    CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink( decryptedtext ) );
+    stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
+    stfDecryptor.MessageEnd();
+
+    //
+    // Dump Decrypted Text
+    //
+    std::cout << decryptedtext;
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    //
+    // String and Sink setup
+    //
+    std::string action = argv[1];
+
+    if (action == "encrypt") {
+      std::string plaintext = argv[2];
+      return encrypt(plaintext);
+    } else if (action == "decrypt") {
+      std::string ciphertext = argv[2];
+      return decrypt(ciphertext);
+    } else {
+      std::cout << "Please use encrypt or decrypt" << std::endl;
+      return 1;
+    }
+}
